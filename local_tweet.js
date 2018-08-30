f8644687752e0f2aa12f5a3e92d5b0b6becce8de
Ellie Frymire <elliefrymire@Ellies-Air.attlocal.net>
2018-08-30 08:55:26 -0700

    it works locally so lets try this

---
+++ local_tweet.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const request = require('request-promise')
+
+const auth = {}
+
+// twitter info
+auth.twitter_oauth = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  token: process.env.USER_TOKEN, // USER SPECIFIC
+  token_secret: process.env.USER_TOKEN_SECRET, // USER SPECIFIC
+}
+
+const tweet = 'Test tweet using the POST statuses/update endpoint'
+
+// request options
+const request_options = {
+  // headers: {
+  //   'content-type': 'application/json'
+  // },
+  oauth: auth.twitter_oauth,
+  // url: 'https://api.twitter.com/1.1/statuses/update.json?in_reply_to_status_id=' + id + '&status=' + encodeURIComponent(tweet),
+  url: 'https://api.twitter.com/1.1/statuses/update.json?status=' + encodeURIComponent(tweet),
+}
+
+// POST request to tweet
+request.post(request_options).then(function (body) {
+  console.log(body)
+}).catch(function (body) {
+  console.log(body)
+})
