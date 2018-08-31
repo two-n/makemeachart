2e131c455bc095733c1b37b33e7392a4ec673a14
Ellie Frymire <elliefrymire@Ellies-Air.attlocal.net>
2018-08-31 10:38:09 -0700

    debugging

--- read_and_reply.js
+++ read_and_reply.js
@@ -56,6 +56,8 @@ const makeViz = (data) => {
 
 module.exports.read_and_reply = read_and_reply = (tweetEvent) => {
 
+	console.log(tweetEvent.tweet_create_events[0].quoted_status.text)
+
 	let quoteStatus = tweetEvent.tweet_create_events[0].is_quote_status
 	let id = tweetEvent.tweet_create_events[0].id_str
 
